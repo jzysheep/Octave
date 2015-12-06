c0223c725868d7a9a82de56008d2905f23afe785
Ahmad El youssef <ahmad@Ahmads-MacBook-Pro.local>
2015-12-06 03:33:57 -0600

    audi NOT complete, code is merged

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,5 @@
 from .home import Music
 from .mainpage import MainPage
-from .mymusic import MyMusic, Reply_Handler, MediaUploadHandler
+from .mymusic import MyMusic, Reply_Handler, MediaUploadHandler,ReplyHandlerAjax,ViewMediaHandler
 from .signup import SignUp
 from .bloburl import MediaUploadFormHandler
-from .mymusic import MyMusic, Reply_Handler, ReplyHandlerAjax
-from .signup import SignUp
